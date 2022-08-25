73279017d7d90b8cdf7393b8ed611f8660aeab0d
ahmad fauzan <fauzan.c4@gmail.com>
2022-08-26 03:00:11 +0700

    get all book

---
+++ index.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost:27017/bwa-graphql', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+// retryWrites: true,
+// w: "majority"
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen()
+    .then(({ url }) => {
+        console.log(`Server ready at ${url}`);
+    });
