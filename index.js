8c3754322179413cd0d645bcf7d843453325a1d1
ahmad fauzan <fauzan.c4@gmail.com>
2022-08-27 04:28:47 +0700

    add plugins graphql playground

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
 
 // connect to mongodb
@@ -11,7 +12,13 @@ mongoose.connect('mongodb://localhost:27017/bwa-graphql', {
     useUnifiedTopology: true,
 });
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground
+    ]
+});
 
 server.listen()
     .then(({ url }) => {
